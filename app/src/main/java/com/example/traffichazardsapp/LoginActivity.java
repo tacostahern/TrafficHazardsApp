37699de8dd7321c99d9aea7babfcb1726364c3bd
tacostahern@gmail.com <tacostahern@gmail.com>
2020-12-07 20:54:51 -0600

    added intent to go from LoginActivity to MainActivity

--- app/src/main/java/com/example/traffichazardsapp/LoginActivity.java
+++ app/src/main/java/com/example/traffichazardsapp/LoginActivity.java
@@ -2,13 +2,34 @@ package com.example.traffichazardsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.login);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
