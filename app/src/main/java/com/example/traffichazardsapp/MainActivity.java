22f92115cce5b8052f55cb2ef996ce9e3fccd84c
aangeles02 <aangeles@hawk.iit.edu>
2020-12-10 20:37:54 -0600

    added buttons to main act

--- app/src/main/java/com/example/traffichazardsapp/MainActivity.java
+++ app/src/main/java/com/example/traffichazardsapp/MainActivity.java
@@ -13,25 +13,41 @@ import android.widget.Spinner;
 public class MainActivity extends AppCompatActivity {
     Button back;
     Spinner Type;
+    Button Submit;
+    Button Upload;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Submit = findViewById(R.id.Submit);
         back = findViewById(R.id.backMain);
         Type = findViewById(R.id.Type);
+        Upload = findViewById(R.id.Upload);
+
+        //Spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Types_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Type.setAdapter(adapter);
-        Intent Back = new Intent(this,LoginActivity.class);
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 toBack();
             }
         });
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toMaps();
+            }
+        });
     }
     public void toBack(){
         Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+    public void toMaps(){
+        Intent intent = new Intent(this,MapsActivity.class);
         startActivity(intent);
     }
 }
