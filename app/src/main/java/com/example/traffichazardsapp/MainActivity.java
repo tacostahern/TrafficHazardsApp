6878b8ec3dd34c3dee974596a259919354c7042c
aangeles02 <aangeles@hawk.iit.edu>
2020-12-10 20:16:11 -0600

    Added spinner on Main Act

--- app/src/main/java/com/example/traffichazardsapp/MainActivity.java
+++ app/src/main/java/com/example/traffichazardsapp/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.traffichazardsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Button back;
+    Spinner Type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        back = findViewById(R.id.backMain);
+        Type = findViewById(R.id.Type);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Types_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Type.setAdapter(adapter);
+        Intent Back = new Intent(this,LoginActivity.class);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toBack();
+            }
+        });
+    }
+    public void toBack(){
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/traffichazardsapp/MapsActivity.java
+++ app/src/main/java/com/example/traffichazardsapp/MapsActivity.java
@@ -2,8 +2,11 @@ package com.example.traffichazardsapp;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,6 +19,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Button Logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Logout = findViewById(R.id.Logout);
+
+        //Logout Button
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toLogin();
+            }
+        });
+    }
+    public void toLogin(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     /**
